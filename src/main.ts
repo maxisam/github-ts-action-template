ae983e3002763186c8655731b24b1245cdb2f26f
maxisam <maxisam@gmail.com>
2022-02-21 23:03:49 -0600

    clean up

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,36 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from '@actions/core';
+import {inspect} from 'util';
+import {getInputs, getOctokit, getOwnerRepo} from './common';
+import {createActionRequest, DispatchEventRequest} from './create-action-request';
 
 async function run(): Promise<void> {
+  const inputs = getInputs();
+  const [owner, repo] = getOwnerRepo(inputs.owner, inputs.repository);
+  const octokit = getOctokit(inputs.authToken, 'github-action');
+  let request: DispatchEventRequest;
+
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    request = createActionRequest(owner, repo);
+    core.debug(`dispatch event request: ${inspect(request)}`);
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.setFailed(`Error creating status request object: ${error.message}`);
+    }
+    return;
+  }
+
+  if (octokit === null) {
+    core.setFailed('Error creating octokit:\noctokit was null');
+  } else {
+    try {
+      await octokit.rest.repos.getViews(request);
+    } catch (error) {
+      core.debug(inspect(error));
+      if (error instanceof Error) {
+        core.setFailed(`Error setting status:\n${error.message}\nRequest object:\n${JSON.stringify(request, null, 2)}`);
+      }
+    }
   }
 }
 
-run()
+run();
